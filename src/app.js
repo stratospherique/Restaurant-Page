3c0cdabffb5ec351b6afb321f80ee63906baafe6
stratospheriquegit config --global user.name stratospheriquegit config --global user.name stratospherique <ahmedosm10@gmail.com>
2019-08-26 00:16:08 +0100

    finish configuring webpack and set dev environment

---
+++ src/app.js
@@ -0,0 +1,5 @@
+import _ from 'lodash';
+import './main.scss';
+
+
+console.log("ssssssssssssssss");
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+  mode: "development",
+  entry: './src/app.js',
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Restaurant Page",
+      template: './src/index.html'
+    })
+  ],
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+           // Creates `style` nodes from JS strings
+           'style-loader',
+           // Translates CSS into CommonJS
+           'css-loader',
+           // Compiles Sass to CSS
+           'sass-loader',
+        ]
+      },
+      {
+        test: /\.(jpg|png|gif|svg)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
+  }
+}
